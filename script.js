9b6edae4aab85d95ac7e37cc3051e9579ef6c24f
Shiielty <kazutonine@gmail.com>
2022-04-13 22:05:23 +0700

    Add the barebone script

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+// Book Constructor:
+function Book(author, title, pages, description, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.description = description;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // take user's input and store the new book object into the myLibrary array.
+}
+
+// TODO: Write a function that loops through the array
+// and display each book on the page (card).
